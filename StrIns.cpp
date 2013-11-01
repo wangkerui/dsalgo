45a531c7270f2c7cb52184dd57ddd0e98f0688ce
wangkerui <wkr@wangkerui.com>
2013-11-01 23:08:28 +0800

    Insert a sub-string to a string
    The C++ solution to http://dsalgo.openjudge.cn/dsmoochw2/2/

---
+++ StrIns.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[14] = {'\0'};
+	char substr[4] = {'\0'};
+	
+	while (cin>>str>>substr) {
+		char *head = str, *curr = str, *max = str;
+		char *tail = str+10;
+		while (curr < tail && *curr != '\0')
+		{
+			if (*curr > *max) max = curr;
+			curr++;
+		}
+		while (curr > max)
+		{
+			*(curr+3) = *curr;
+			curr--;
+		}
+
+		*(curr+1) = substr[0];
+		*(curr+2) = substr[1];
+		*(curr+3) = substr[2];
+		
+		cout<<str<<endl;
+
+	}
+
+	return 0;
+}
